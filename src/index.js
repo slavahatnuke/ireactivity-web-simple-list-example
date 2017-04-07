18c5cab30ca1c12a08c300c442de3b6718ae06e2
slava hatnuke <slava.hatnuke@gmail.com>
2017-04-08 02:40:50 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Provider, connect} from 'ireactivity';
+const uid = () => Math.random().toString(35).slice(2, 8).toUpperCase();
+
+const store = {
+    todos: [
+        {title: 'Todo #1', id: uid()},
+        {title: 'Todo #2', id: uid()}
+    ]
+};
+
+const TodoView = ({todo, onRemove}) =>
+    <div>
+        <button onClick={() => onRemove(todo)}>x</button>
+        {todo.title}
+    </div>
+
+const Todo = connect(TodoView, {
+    onRemove: (store) => (todo) => {
+        store.todos = store.todos.filter((aTodo) => todo !== aTodo)
+    }
+});
+
+const TodosView = ({todos}) =>
+    <div>
+        {todos.map((todo) => <Todo key={todo.id} todo={todo}/>)}
+    </div>;
+
+const Todos = connect(TodosView, {
+    todos: (store) => store.todos
+});
+
+const TodoPlusView = ({onClick}) => <a href="#" onClick={onClick}>[ + ]</a>;
+
+const TodoPlus = connect(TodoPlusView, {
+    onClick: (store) => () => {
+        let id = uid();
+        store.todos = [...store.todos, {title: `Todo #${id}`, id: id}]
+    }
+});
+
+const AppView = () => <div><h1>Todos</h1> <TodoPlus/> <Todos/></div>;
+
+const App = AppView;
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
